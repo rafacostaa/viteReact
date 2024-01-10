0ea4b40bef2d0ddc7361461fd158654f329541b7
rafaelrgc <rafargc@gmail.com>
2024-01-10 08:11:56 -0300

    vite config test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: process.env.NODE_ENV === 'production' ? 'url' : '/',
+  base:
+    process.env.NODE_ENV === "production"
+      ? "https://vitereacttest2.netlify.app/"
+      : "/",
 });
